d15e071e2efe5c4009638b39f481dd8b1e6b0468
leiyufang <leiyufang@sunlands.com>
2020-04-07 14:50:05 +0800

    ssmdemo

--- ssmdemo/src/main/java/cn/sd/ssmdemo/service/BaseService.java
+++ ssmdemo/src/main/java/cn/sd/ssmdemo/service/BaseService.java
@@ -1,4 +1,18 @@
 package cn.sd.ssmdemo.service;
 
-public interface BaseService {
+import java.util.List;
+
+public interface BaseService<T> {
+	//增删改查
+	
+	public void add(T t);
+	
+	public void delete(int id);
+	
+	public void update(T t);
+	
+	public T select(int id);
+	
+	public List<T> selectall();
+	
 }
