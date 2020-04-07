d8d64aee87fe85ced278ee2378ff3a302bb8a0b7
leiyufang <leiyufang@sunlands.com>
2020-04-07 14:21:38 +0800

    ssmdemo

--- ssmdemo/src/test/java/SsmdemoTest.java
+++ ssmdemo/src/test/java/SsmdemoTest.java
@@ -1,8 +1,16 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.sql.DataSource;
 
 public class SsmdemoTest {
 	@Test
 	public  void ssmTest(){
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		DataSource dataSource = (DataSource) context.getBean("dataSource");
+		System.out.println(dataSource.getClass());
 	
 	}
 }
