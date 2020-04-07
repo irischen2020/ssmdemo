f4b7d1a3d47a37ab01e767c18df7110cf4bb6d03
leiyufang <leiyufang@sunlands.com>
2020-04-07 14:31:44 +0800

    ssmdemo

--- ssmdemo/src/main/java/cn/sd/ssmdemo/dao/BaseDao.java
+++ ssmdemo/src/main/java/cn/sd/ssmdemo/dao/BaseDao.java
@@ -1,4 +1,20 @@
 package cn.sd.ssmdemo.dao;
 
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
 public interface BaseDao {
+	//增删改查
+	public void add(@Param("tableName") String tableName,@Param("objects") Object... objects);
+	
+	public void delete(@Param("tableName") String tableName,@Param("id") Integer id);
+	
+	public void update(@Param("tableName") String tableName,@Param("id") Integer id,@Param("objects") Object[] objects);
+	
+	//查询一条记录
+	public Map<Object,Object> select(@Param("tableName") String tableName,@Param("id") Integer id);
+	//查询所有记录
+	public List<Map<Object,Object>> selectall(@Param("tableName") String tableName);
 }
