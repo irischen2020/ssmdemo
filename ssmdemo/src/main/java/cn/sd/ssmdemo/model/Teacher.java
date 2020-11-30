ac9e313428234748dacbfb25aa27f9dc156d2d63
leiyufang <leiyufang@sunlands.com>
2020-11-30 18:57:54 +0800

    ssmdemo

---
+++ ssmdemo/src/main/java/cn/sd/ssmdemo/model/Teacher.java
@@ -0,0 +1,8 @@
+package cn.sd.ssmdemo.model;
+
+public class Teacher {
+	private Integer id;
+	private String name;
+	private String coursename;
+	
+}
---
+++ ssmdemo/src/test/java/MyBatisTest2.java
@@ -0,0 +1,25 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 演示动态SQL效果
+ */
+public class MyBatisTest2 {
+	
+	private SqlSessionFactory sqlSessionFactory;
+	
+	@Before
+	public void init() throws IOException {
+		//		1、根据全局配置文件创建出一个SqlSessionFactory
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	}
+	
+	
+}
